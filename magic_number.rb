a1a6f5cf7694f41058d7e7042b0137d76a17c332
Nick Ames <nickrames@gmail.com>
2017-12-01 13:29:17 -0700

    feature complete

--- magic_number.rb
+++ magic_number.rb
@@ -6,31 +6,21 @@ require 'to_words'
 
 def question
   puts "please input a positive number or type QUIT to exit"
-  @answer = gets.strip.downcase
-  if @answer == "quit"
-    puts "Thanks for playing. Goodbye!"
-    exit
-  else
-    conclusion
-  end
+  answer = gets.strip
+  exit if answer.downcase == "quit"
+  answer.to_i.abs
 end  
 
-def conclusion
-  puts "#{@answer} is #{@answer.to_words}"
-  puts "and #{@response} is the magic number"
-  puts @response
-  puts "Type QUIT to exit, or enter another number."  
-  choice
-end
-
-def choice
-  response = gets.strip.to_s.downcase
-  if response == "quit"
-    puts "Thanks for playing. Goodbye!"
-    exit
+def magic_number(number)
+  num_length = number.to_words.length
+  if num_length == 4
+    puts "#{number} is #{num_length} and #{num_length} is the magic number"
   else
-    conclusion
+    puts "#{number} is #{num_length}"
+    magic_number(num_length)
   end
 end
 
-question
+while true
+  magic_number(question)
+end
